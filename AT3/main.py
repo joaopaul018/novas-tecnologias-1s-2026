3084f2e04451df1bc3905a31d965387fc442ded4
João Paulo Batista Dos Santos <162514744+joaopaul018@users.noreply.github.com>
2026-04-17 14:17:12 -0300

    Create main.py

---
+++ AT3/main.py
@@ -0,0 +1,10 @@
+from matrix import transpor_matriz, multiplicar_matriz
+
+
+A = [[1, 2], [3, 4], [5, 6]]
+print(transpor_matriz(A))
+
+
+A = [[1, 2], [3, 4]]
+B = [[5, 6], [7, 8]]
+print(multiplicar_matriz(A, B))
